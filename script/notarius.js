138bc6240924ddd5364e4332a95125fb6ca3be96
dimitrrrr <dima_romanchuk@ukr.net>
2021-06-28 20:36:57 +0300

    added notarius search and other minor fixes

--- script/notarius.js
+++ script/notarius.js
@@ -1,4 +1,4 @@
-var mymap = L.map("mapid").setView([46.5525, 30.7533], 13);
+var mymap = L.map("mapid").setView([46.5525, 30.7533], 7);
 
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -16,13 +16,237 @@ var marker3 = L.marker([45.343545, 28.837576]);
 marker3.bindPopup('Ефимова Татьяна Ивановна').openPopup();
 marker3.addTo(mymap);
 
-var marker4 = L.marker([47.714758, 29.991326]);
+var marker4 = L.marker([47.71, 30.0021]);
 marker4.bindPopup('Ананьевская районная нотариальная контора').openPopup();
 marker4.addTo(mymap);
 
 var marker5 = L.marker([46.185452, 30.341491]);
 marker5.bindPopup('Белгород-Днестровская городская нотариальная контора').openPopup();
 marker5.addTo(mymap);
+
+var marker6 = L.marker([47.714758, 29.901326]);
+marker6.bindPopup('Воробьев Анатолий Алексеевич').openPopup();
+marker6.addTo(mymap);
+
+var marker7 = L.marker([45.978844, 29.423555]);
+marker7.bindPopup('Бучацкий Владимир Яковлевич').openPopup();
+marker7.addTo(mymap);
+
+var marker8 = L.marker([46.004229, 29.427333]);
+marker8.bindPopup('Арцизская районная нотариальная контора').openPopup();
+marker8.addTo(mymap);
+
+var marker9 = L.marker([45.976, 29.433555]);
+marker9.bindPopup('Мицул Оксана Вадимовна').openPopup();
+marker9.addTo(mymap);
+
+var marker10 = L.marker([47.937202, 29.623496]);
+marker10.bindPopup('Пантилеева Инна Петровна').openPopup();
+marker10.addTo(mymap);
+
+var marker11 = L.marker([47.937087, 29.628840]);
+marker11.bindPopup('Смирнова Наталья Станиславовна').openPopup();
+marker11.addTo(mymap);
+
+var marker12 = L.marker([47.200826, 30.914978]);
+marker12.bindPopup('Мухаир Валентина Тимофеевна').openPopup();
+marker12.addTo(mymap);
+
+var marker13 = L.marker([47.203827, 30.916348]);
+marker13.bindPopup('Березовская районная нотариальная контора').openPopup();
+marker13.addTo(mymap);
+
+var marker14 = L.marker([47.200680, 30.906750]);
+marker14.bindPopup('Березовская Елена Григорьевна').openPopup();
+marker14.addTo(mymap);
+
+var marker15 = L.marker([46.194846, 30.342263]);
+marker15.bindPopup('Чухрай Татьяна Юрьевна').openPopup();
+marker15.addTo(mymap);
+
+var marker16 = L.marker([46.196769, 30.357692]);
+marker16.bindPopup('Белгород-Днестровская районная нотариальная контора').openPopup();
+marker16.addTo(mymap);
+
+var marker17 = L.marker([46.190535, 30.340714]);
+marker17.bindPopup('Титова Татьяна Сергеевна').openPopup();
+marker17.addTo(mymap);
+
+var marker18 = L.marker([46.194411, 30.346599]);
+marker18.bindPopup('Чербаджи Екатерина Николаевна').openPopup();
+marker18.addTo(mymap);
+
+var marker19 = L.marker([46.155309, 30.339858]);
+marker19.bindPopup('Драченко Анатолий Никифорович').openPopup();
+marker19.addTo(mymap);
+
+var marker20 = L.marker([46.478877, 30.200898]);
+marker20.bindPopup('Беляевская районная нотариальная контора').openPopup();
+marker20.addTo(mymap);
+
+var marker21 = L.marker([46.479691, 30.201185]);
+marker21.bindPopup('Галайко Оксана Дмитриевна').openPopup();
+marker21.addTo(mymap);
+
+var marker22 = L.marker([46.481757, 30.201405]);
+marker22.bindPopup('Зезик Вячеслав Андреевич').openPopup();
+marker22.addTo(mymap);
+
+var marker23 = L.marker([46.581834, 30.545955]);
+marker23.bindPopup('Кифоренко Руслана Борисовна').openPopup();
+marker23.addTo(mymap);
+
+var marker24 = L.marker([46.539887, 30.659589]);
+marker24.bindPopup('Гуменюк Андрей Николаевич').openPopup();
+marker24.addTo(mymap);
+
+var marker25 = L.marker([46.473599, 30.558221]);
+marker25.bindPopup('Курганская Лилия Валерьевна').openPopup();
+marker25.addTo(mymap);
+
+var marker26 = L.marker([45.685861, 28.607824]);
+marker26.bindPopup('Станева Валентина Борисовна').openPopup();
+marker26.addTo(mymap);
+
+var marker27 = L.marker([45.705861, 28.608221]);
+marker27.bindPopup('Ажинтарев Виталий Владимирович').openPopup();
+marker27.addTo(mymap);
+
+var marker28 = L.marker([47.085337, 29.854806]);
+marker28.bindPopup('Великомихайловская районная нотариальная контора').openPopup();
+marker28.addTo(mymap);
+
+var marker29 = L.marker([47.333568, 29.760391]);
+marker29.bindPopup('Захаровская районная нотариальная контора').openPopup();
+marker29.addTo(mymap);
+
+var marker30 = L.marker([46.975524, 30.469358]);
+marker30.bindPopup('Дучко Вероника Николаевна').openPopup();
+marker30.addTo(mymap);
+
+var marker31 = L.marker([45.452522, 29.268884]);
+marker31.bindPopup('Килийская районная нотариальная контора').openPopup();
+marker31.addTo(mymap);
+
+var marker32 = L.marker([48.092112, 29.123183]);
+marker32.bindPopup('Кодымская районная нотариальная контора').openPopup();
+marker32.addTo(mymap);
+
+var marker33 = L.marker([46.821014, 30.945790]);
+marker33.bindPopup('Белоконь Александра Никитична').openPopup();
+marker33.addTo(mymap);
+
+var marker34 = L.marker([46.584633, 30.803361]);
+marker34.bindPopup('Полякова Анна Владимировна').openPopup();
+marker34.addTo(mymap);
+
+var marker35 = L.marker([47.083333, 30.033333]);
+marker35.bindPopup('Любашевская районная нотариальная контора').openPopup();
+marker35.addTo(mymap);
+
+var marker36 = L.marker([47.540994, 30.759191]);
+marker36.bindPopup('Сологуб Олег Анатольевич').openPopup();
+marker36.addTo(mymap);
+
+var marker37 = L.marker([46.251999, 30.437288]);
+marker37.bindPopup('Мирошниченко Наталья Николаевна').openPopup();
+marker37.addTo(mymap);
+
+var marker38 = L.marker([46.332983, 30.565545]);
+marker38.bindPopup('Русинова Антонина Евгеньевна').openPopup();
+marker38.addTo(mymap);
+
+var marker39 = L.marker([46.345626, 30.690818]);
+marker39.bindPopup('Гурская Валентина Семеновна').openPopup();
+marker39.addTo(mymap);
+
+var marker40 = L.marker([47.542259, 29.455235]);
+marker40.bindPopup('Бушуева Ирина Игоревна').openPopup();
+marker40.addTo(mymap);
+
+var marker41 = L.marker([47.746661, 29.533090]);
+marker41.bindPopup('Подольская районная нотариальная контора').openPopup();
+marker41.addTo(mymap);
+
+var marker42 = L.marker([45.455303, 28.277662]);
+marker42.bindPopup('Ренийская районная нотариальная контора').openPopup();
+marker42.addTo(mymap);
+
+var marker43 = L.marker([46.854624, 30.074944]);
+marker43.bindPopup('Фишер Борис Иванович').openPopup();
+marker43.addTo(mymap);
+
+var marker44 = L.marker([46.189188, 29.146495]);
+marker44.bindPopup('Тарутинская районная нотариальная контора').openPopup();
+marker44.addTo(mymap);
+
+var marker45 = L.marker([47.074526, 30.911406]);
+marker45.bindPopup('Белобров Павел Петрович').openPopup();
+marker45.addTo(mymap);
+
+var marker46 = L.marker([45.840445, 29.625971]);
+marker46.bindPopup('Татарбунары районная нотариальная контора').openPopup();
+marker46.addTo(mymap);
+
+var marker47 = L.marker([46.505803, 30.322121]);
+marker47.bindPopup('Теплодарская городская нотариальная контора').openPopup();
+marker47.addTo(mymap);
+
+var marker48 = L.marker([46.306430, 30.659645]);
+marker48.bindPopup('Слаева Раиса Каримовна').openPopup();
+marker48.addTo(mymap);
+
+var marker49 = L.marker([47.375494, 30.195835]);
+marker49.bindPopup('Петров Юрий Николаевич').openPopup();
+marker49.addTo(mymap);
+
+var marker50 = L.marker([46.624541, 31.098878]);
+marker50.bindPopup('Буньковская Светлана Сергеевна').openPopup();
+marker50.addTo(mymap);
+
+var marker51 = L.marker([46.485092, 30.738957]);
+marker51.bindPopup('1-я Одесская нотариальная контора').openPopup();
+marker51.addTo(mymap);
+
+var marker52 = L.marker([46.474554, 30.739156]);
+marker52.bindPopup('2-я Одесская нотариальная контора').openPopup();
+marker52.addTo(mymap);
+
+var marker53 = L.marker([46.485297, 30.737542]);
+marker53.bindPopup('3-я Одесская нотариальная контора').openPopup();
+marker53.addTo(mymap);
+
+var marker51 = L.marker([46.492224, 30.720524]);
+marker51.bindPopup('5-я Одесская нотариальная контора').openPopup();
+marker51.addTo(mymap);
+
+var marker52 = L.marker([46.472390, 30.738851]);
+marker52.bindPopup('6-я Одесская нотариальная контора').openPopup();
+marker52.addTo(mymap);
+
+var marker53 = L.marker([46.435730, 30.704692]);
+marker53.bindPopup('7-я Одесская нотариальная контора').openPopup();
+marker53.addTo(mymap);
+
+var marker51 = L.marker([46.584147, 30.783910]);
+marker51.bindPopup('8-я Одесская нотариальная контора').openPopup();
+marker51.addTo(mymap);
+
+var marker52 = L.marker([46.462834, 30.742120]);
+marker52.bindPopup('Петровский Валерий Михайлович').openPopup();
+marker52.addTo(mymap);
+
+var marker53 = L.marker([46.491886, 30.714855]);
+marker53.bindPopup('Алексеева Елена Александровна').openPopup();
+marker53.addTo(mymap);
+
+var marker54 = L.marker([46.390462, 30.724405]);
+marker54.bindPopup('Поведенкова Инна Ивановна').openPopup();
+marker54.addTo(mymap);
+
+var marker55 = L.marker([46.425396, 30.751760]);
+marker55.bindPopup('Луцях Татьяна Николаевна').openPopup();
+marker55.addTo(mymap);
 
 $('#locate-position').on('click', function () {
     mymap.locate({
@@ -33,7 +257,17 @@ $('#locate-position').on('click', function () {
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
-    L.marker(e.latlng).addTo(mymap);
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    L.marker(e.latlng, {
+        icon: greenIcon
+    }).addTo(mymap);
     L.circle(e.latlng, radius).addTo(mymap);
 }
 
@@ -71,3 +305,16 @@ function sortTable(table, colNum, type) {
     rowsArray.sort(compare);
     tbody.append(...rowsArray);
 }
+
+$('.find input').on('input', function () {
+    debugger;
+    $('td').each(function () {
+        $(this).parent().show();
+    });
+    let value = $(this).val();
+    $('td:nth-child(2)').each(function () {
+        let a = $(this).html().toLowerCase();
+        if (!a.startsWith(value.toLowerCase()))
+            $(this).parent().hide();
+    });
+});
